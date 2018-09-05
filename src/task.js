082f26cb11e1b7444183d3f84fe53357a342c6d8
Ali Aziz <aliaziz@Alis-MacBook-Pro.local>
2018-09-05 10:33:46 +0100

    Done.

--- src/task.js
+++ src/task.js
@@ -1,2 +1,14 @@
 class Task {
+  constructor(text, id){
+    this.text = text
+    this.id = id
+  }
+
+  render (){
+    const task = document.createElement('li')
+    task.id = `todo-${this.id}`
+    task.innerText = this.text
+    return task
+  }
+
 }
