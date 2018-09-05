da998a06f423c711b5badfb98c739f267c82ff76
ritz-w <ritz.wu@outlook.com>
2018-09-05 17:43:26 +0100

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,12 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+  window.taskList = taskList
+
+  document.getElementById('create-task-form').addEventListener('submit', function(event) {
+    event.preventDefault()
+    taskList.addTask()
+    document.getElementById('new-task-description').value = ""
+    })
+
 });
